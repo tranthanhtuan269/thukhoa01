9e3c7e74a0ef20b3e87af65e51ee6ea67a1e91a3
tran.thanh.tuan269 <tran.thanh.tuan269@gmail.com>
2015-10-15 09:18:15 +0700

    add files

---
+++ protected/views/hosocanhan/index.php
@@ -0,0 +1,15 @@
+<?php
+/* @var $this HosocanhanController */
+
+$this->breadcrumbs=array(
+	'Hosocanhan',
+);
+?>
+<h1><?php echo $this->id . '/' . $this->action->id; ?></h1>
+
+<p>
+	You may change the content of this page by modifying
+	the file <tt><?php echo __FILE__; ?></tt>.
+</p>
+
+<?php var_dump($this); ?>
